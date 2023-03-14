54f2a953bffa7cbf3b0cd822b394ab3da67b863f
dennis kimani <denniskimani416@gmail.com>
2023-03-14 12:18:06 +0300

    completed assignment on review strings

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = "Alice";
+let welcomeMessage = "Welcome to Flatbook!";
+welcomeMessage = `${welcomeMessage.slice(0, -1)}, ${currentUser}!`;
+const excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, We are exited to have you!`
+const shortGreeting =`Welcome, ${currentUser.slice(0, 1)}!`;
